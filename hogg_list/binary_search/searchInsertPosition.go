6284f3df0f371d5937dda5b0b7def7b81ee00109
whotterre <ijeddy5.0@gmail.com>
2026-01-16 14:01:13 +0100

    add solution to Problem 35: Search Insert Position

---
+++ hogg_list/binary_search/searchInsertPosition.go
@@ -0,0 +1,21 @@
+package main
+
+func searchInsert(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := low + (high - low) / 2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return low
+} 
