1b58ca74001ca38cbaba2d60366cff5ad1331a4a
whotterre <ijeddy5.0@gmail.com>
2026-01-17 11:36:27 +0100

    add solution to 83: Remove Duplicates From Sorted List

---
+++ hogg_list/linked_list/removeDuplicatesFromSortedList.go
@@ -0,0 +1,20 @@
+package main 
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+    curr := head 
+    for curr != nil && curr.Next != nil {
+        if curr.Val == curr.Next.Val {
+            curr.Next = curr.Next.Next
+        } else {
+            curr = curr.Next
+        }
+    }
+    return head
+}
